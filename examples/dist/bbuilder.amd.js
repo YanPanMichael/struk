9920ea3d129b76c4de06153c8498f7bcac71f193
panyan <panyan@bilibili.com>
2021-08-16 18:21:50 +0800

    chore(release): 0.2.1

---
+++ examples/dist/bbuilder.amd.js
@@ -0,0 +1,23 @@
+/*!
+        * with v0.2.0
+        * Author: panyan <panyan@bilibili.com>
+        * Built on 2021-08-16, 16:26:31
+        * Released under the ISC License Copyright (c) 2021-2021
+        */
+define(['lodash'], function (_) { 'use strict';
+
+    function _interopDefaultLegacy (e) { return e && typeof e === 'object' && 'default' in e ? e : { 'default': e }; }
+
+    var ___default = /*#__PURE__*/_interopDefaultLegacy(_);
+
+    function B() {
+      console.log('test typescript');
+
+      ___default['default'].assign({}, {
+        name: 'test'
+      });
+    }
+
+    B();
+
+});
